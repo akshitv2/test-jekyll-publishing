1fa30e43ff29c9b0ee6bd8e69fcf4b573e608799
Akshit <ak.bisht1@gmail.com>
2026-04-04 23:47:56 +0530

    Update scripts + publish new pages

--- Publisher.py
+++ Publisher.py
@@ -8,12 +8,16 @@ from publishingLibs.FileUtil import get_file_content, write_public_page, convert
 from publishingLibs.CryptoUtil import get_key, encrypt
 from publishingLibs.LogUtil import LogUtil
 from publishingLibs.WebUtil import get_rendered_html
+import json
 
 config = {
     'original_folder': "./original",  # Change this to your path
     'output_folder': "./pages",  # Change this to your path
     'key':get_key().encode()[:32],
-    'log_level': 'INFO'
+    'log_level': 'INFO',
+    'media_json_input': 'original/media.json',
+    'media_json_output': 'pages/media.json',
+    'process_md': True
 }
 log = LogUtil(config["log_level"])
 
@@ -22,18 +26,26 @@ def change_dir_to_current_file():
     log.info(("Current working directory:", os.getcwd()))
 change_dir_to_current_file()
 print(os.getcwd())
+if config["process_md"]:
+    for filename in os.listdir(config["original_folder"]):
+        if not filename.endswith(".md"):
+            continue
+        # get original content of file to store for easy recovery
+        file_content = get_file_content(os.path.join(config["original_folder"], filename))
+        log.debug(("File content:", file_content))
+        # convert to base64 for simple easy format
+        base64_string = convert_md_to_base64(file_content)
+        log.debug(("Base 64 String:", base64_string))
 
-for filename in os.listdir(config["original_folder"]):
-    # get original content of file to store for easy recovery
-    file_content = get_file_content(os.path.join(config["original_folder"], filename))
-    log.debug(("File content:", file_content))
-    # convert to base64 for simple easy format
-    base64_string = convert_md_to_base64(file_content)
-    log.debug(("Base 64 String:", base64_string))
+        html_content_string = get_rendered_html('http://localhost:4000/original/', filename)
+        log.debug(("Content string:", html_content_string))
 
-    html_content_string = get_rendered_html('http://localhost:4000/original/', filename)
-    log.debug(("Content string:", html_content_string))
+        data = html_content_string + '<div id="b64Container">' + base64_string + '</div>'
+        iv, ct_bytes = encrypt(config["key"], data)
+        write_public_page(config["output_folder"], filename, file_content, iv, ct_bytes)
 
-    data = html_content_string + '<div id="b64Container">' + base64_string + '</div>'
-    iv, ct_bytes = encrypt(config["key"], data)
-    write_public_page(config["output_folder"], filename, file_content, iv, ct_bytes)
+with open(config['media_json_input'], 'r') as f:
+    data = json.load(f)
+lowercase_data = {k: encrypt(config["key"], v) for k, v in data.items()}
+with open(config['media_json_output'], 'w') as f:
+    json.dump(lowercase_data, f, indent=4)
